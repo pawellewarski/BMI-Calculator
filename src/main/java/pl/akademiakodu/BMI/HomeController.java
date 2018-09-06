8ffbb025aa255169a71d772e7924d947475facd3
pawellewarski <pawellewarski@gmail.com>
2018-09-06 20:38:29 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/BMI/HomeController.java
@@ -0,0 +1,16 @@
+package pl.akademiakodu.BMI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "home";
+    }
+
+
+}
